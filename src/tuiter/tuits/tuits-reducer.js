811b417de6fb4861dbf39f8617a50a71916b741f
liamgabrielknowles <liamgabrielknowles2@gmail.com>
2022-11-18 14:52:41 -0500

    f
    f

--- src/tuiter/tuits/tuits-reducer.js
+++ src/tuiter/tuits/tuits-reducer.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { createSlice } from "@reduxjs/toolkit";
 import tuits from './tuits.json';
 import {deleteTuitThunk, findTuitsThunk, createTuitThunk, updateTuitThunk}
